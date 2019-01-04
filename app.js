3784103d1f59ae02c47a397c2aeb0fafff676ac7
zarab <musfiqzarab@iut-dhaka.edu>
2019-01-04 21:05:54 +0600

    Adding basic route

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 
 const app = express(); //initialize application
+
+// Index route
+app.get("/", (req, res) => {
+  res.send("INDEX!");
+});
+
+// About route
+app.get("/about", (req, res) => {
+  res.send("ABOUT");
+});
+
+// port initialization
 const port = 5000;
 
 app.listen(port, () => {
