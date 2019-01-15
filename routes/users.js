6a7f3f2357cff0c5b3f1e7d82c0be28e5b09d7a7
zarab <musfiqzarab@iut-dhaka.edu>
2019-01-15 23:04:30 +0600

    Register Form Validation

--- routes/users.js
+++ routes/users.js
@@ -4,17 +4,32 @@ const mongoose = require("mongoose");
 
 //User Login Route
 router.get("/login", (req, res) => {
-  res.render("users/login");
+  res.render("./users/login");
 });
 //User Register Route
 router.get("/register", (req, res) => {
-  res.render("users/register");
+  res.render("./users/register");
 });
 
 //Register Form  Post
-// router.post("/register", (req, res) => {
-//   console.log(req.body);
-//   res.send("register");
-// });
+router.post("/register", (req, res) => {
+  let errors = [];
+  if (req.body.password !== req.body.password2) {
+    errors.push({ text: `Passwords Don't Match` });
+  }
+  if (req.body.password.length < 4) {
+    errors.push({ text: `Password Must be at least 4 Characters` });
+  }
+  if (errors.length > 0) {
+    res.render("./users/register", {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+  } else {
+    res.send("passed");
+  }
+});
 
 module.exports = router;
